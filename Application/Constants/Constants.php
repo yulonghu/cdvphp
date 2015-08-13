6cae53269bd4ccb06a2d411c2e977874621ae90f
fanjiapeng <fanjiapeng@360.cn>
2015-08-13 23:33:17 +0800

    增加API、PHP模式

---
+++ Application/Constants/Constants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 框架入门 - 常量错误码、常量错误信息定义
+ *
+ * 常用于客户端与服务端接口交互(Android、Iphone、Winform)
+ *
+ * @link http://www.cdvphp.com
+ * @author <fanjiapeng@126.com>
+ * @package Application\Controller
+ */
+class Constants
+{
+    const ERR_TEST = 1000;
+
+    static public $ErrorDescription = array(
+        self::ERR_TEST => '测试, 系统默认错误',
+    );
+}
---
+++ Application/Controller/Api.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * 框架入门 - serverAPI
+ *
+ * 常用于客户端与服务端接口交互(Android、Iphone、Winform)
+ *
+ * @link http://www.cdvphp.com
+ * @author <fanjiapeng@126.com>
+ * @package Application\Controller
+ */
+class ApiController
+{
+    /**
+     * 自定义正确输出json
+     *
+     * http://域名/index.php?method=api.index
+     *
+     * @return json
+     */
+    public function index()
+    {/*{{{*/
+    }/*}}}*/
+
+    /**
+     * 自定义正确输出json
+     *
+     * http://域名/index.php?method=api.data
+     *
+     * @return json
+     */
+    public function data()
+    {/*{{{*/
+        return array('user' => 'sanmao', 'age' => 20);
+    }/*}}}*/
+
+    /**
+     * 自定义错误输出json
+     *
+     * http://域名/index.php?method=api.BizResult
+     *
+     * @return json
+     */
+    public function bizResult()
+    {/*{{{*/
+        BizResult::ensureTrue(false, 1000);
+    }/*}}}*/
+}
